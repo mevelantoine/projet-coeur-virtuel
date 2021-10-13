2261328a43d1aa994a20a6030a0d5706c604c2da
Antoine MEVEL <mevelantoi@eisti.eu>
2021-10-13 15:37:29 +0200

    Test de lecture

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 //Les registres correspondent à des long (64bits)
 //Le PC est un long (vu qu'il correspond à une adresse)
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char **argv){
+    FILE *input;
     
+    input = fopen(argv[1],"r");
+
+    printf("%s",input);
+    fclose(input);
 }
